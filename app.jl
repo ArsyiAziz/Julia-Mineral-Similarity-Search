dd842320ca69bfb8e8a64cf48458e9edef19d380
ArsyiAziz <arsyiaziz@gmail.com>
2024-11-13 08:48:15 -0500

    update layout

--- app.jl
+++ app.jl
@@ -1,7 +1,7 @@
 using Dash
 using CSV, DataFrames
 
-external_stylesheets = []
+external_stylesheets = ["css/style.css"]
 external_scripts = [
     Dict("src"=>"https://cdn.tailwindcss.com"),
     # Dict("src"=>"https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js")
@@ -14,66 +14,80 @@ df[!," index"] = 1:nrow(df)
 
 visible_columns = ["Name", "Crystal Structure", "Mohs Hardness", "Diaphaneity", "Specific Gravity", "Optical", "Refractive Index", "Dispersion"]
 
-app.layout = html_div(className="container py-5 mx-auto px-4") do
-    html_h1("Mineral Similarity Search", className="text-2xl"),
-    dcc_graph(),
-    html_div(className="flex space-x-4 justify-between mb-2") do
-        [
-            html_div(className="flex flex-col") do
-                [
-                    html_label("Entries per page"),
-                    dcc_input(
-                        id="datatable-page-size",
-                        type="number",
-                        min=1,
-                        max=20,
-                        value=10,
-                        className="rounded border border-slate-300 hover:border-slate-400 ps-1"
-                    )
-                ]
-            end,
-            html_div(className="flex flex-col") do 
-                [
-                    html_label("Search"),
+app.layout = html_div([
+    html_div(className="px-10 py-6 mx-auto bg-[#52525b] w-screen", [
+        html_h1("Mineral Similarity Search", className="text-2xl font-mono text-white")
+    ]),
+    html_div(className="container mx-auto flex flex-col space-y-6 my-4", [
+        html_div(className="px-8 py-6 rounded rounded-md bg-white", [
+        html_h2("Mineral Database", className="text-xl font-bold"),
+        html_div(className="flex space-x-4 justify-between mb-2", 
+            [
+                html_div(className="flex flex-col", [
+                        html_label("Entries per page"),
+                        dcc_input(
+                            id="datatable-page-size",
+                            type="number",
+                            min=5,
+                            max=20,
+                            value=10,
+                            className="rounded border border-slate-300 hover:border-slate-400 ps-2 rounded-full"
+                        )
+                ]),
+                html_div(className="flex flex-col-reverse", [
                     dcc_input(
                         id="datatable-search", 
                         value="", 
                         debounce=false,
-                        className="rounded border border-slate-300 hover:border-slate-400 ps-1",
-                        placeholder="Mineral name"
+                        className="rounded border border-slate-300 hover:border-slate-400 ps-2 rounded-full",
+                        placeholder="Search mineral"
                     ) 
-                ]
-            end,
-        ]
-    end,
-    dash_datatable(
-        id="datatable",
-        columns=[Dict("name" => c, "id" => c) for c in visible_columns],
-        page_current=0,
-        page_action="custom",
-        row_selectable="multi",
-        column_selectable="single",
-        selected_columns=[],
-        selected_rows=[],
-        style_table=Dict(
-                    "minWidth" => "100%",      # Ensures table stretches to full width
-                    "overflowX" => "auto"      # Enables horizontal scroll
+                ]),
+        ]),
+        dash_datatable(
+            id="datatable",
+            columns=[Dict("name" => c, "id" => c) for c in visible_columns],
+            page_current=0,
+            page_action="custom",
+            row_selectable="multi",
+            column_selectable="single",
+            selected_columns=[],
+            selected_rows=[],
+            style_table=Dict(
+                        "minWidth" => "100%",      # Ensures table stretches to full width
+                        "overflowX" => "auto"      # Enables horizontal scroll
+            ),
+            style_cell=Dict(
+                "textAlign" => "left",     # Left-align text
+                # "minWidth" => "150px",     # Minimum width for columns
+                "width" => "150px",        # Width of columns
+                "maxWidth" => "300px",     # Max width for larger screens
+                "whiteSpace" => "normal",  # Wrap text instead of truncating
+            ),
+            style_header=Dict(
+                "fontWeight" => "bold"     # Bold header text
+            ),
+            # style_data=Dict("border"=>"1px solid black"),
+            # style_header=Dict("border"=>"1px solid black"),
         ),
-        style_cell=Dict(
-            "textAlign" => "left",     # Left-align text
-            # "minWidth" => "150px",     # Minimum width for columns
-            "width" => "150px",        # Width of columns
-            "maxWidth" => "300px",     # Max width for larger screens
-            "whiteSpace" => "normal",  # Wrap text instead of truncating
-        ),
-        style_header=Dict(
-            "fontWeight" => "bold"     # Bold header text
-        ),
-        # style_data=Dict("border"=>"1px solid black"),
-        # style_header=Dict("border"=>"1px solid black"),
-    ),
-    html_button("Calculate cosine similarity", className="rounded border border-slate-300 p-2 bg-[#65a30d] text-white")
-end
+        html_button("Reset", className="rounded border border-slate-300 p-2 bg-[#475569] text-white px-6"),
+        html_button("Calculate", className="rounded border border-slate-300 p-2 bg-[#2563eb] text-white px-6"),
+        ]),
+        
+        html_div(className="px-8 py-6 rounded rounded-md bg-white", [
+            html_h2("Similarity Summary", className="text-xl font-bold"),
+            html_div(className="flex flex-row mb-6", [
+                html_button("Cosine", className="border-b-4 border-[#2563eb] px-2"),
+                html_button("Jaccard", className="border-b-4 border-gray px-2"),
+                html_button()
+            ]),
+            
+            html_div([html_h3("Selected Mineral:", className="pe-2"), html_div([html_div("Mineral 1", className="rounded-md border border-black px-2")], id="selected-minerals")], className="flex flex-row"),
+            
+            dcc_graph()
+        ])
+    ])
+])
 
 # Callback to update the table data based on page, search input, and update page count
 callback!(app,
